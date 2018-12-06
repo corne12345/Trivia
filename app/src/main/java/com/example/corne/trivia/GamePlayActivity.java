4c3c4bcf7154147f8ffb1cfb50e829c232a3d454
Corne <39339251+corne12345@users.noreply.github.com>
2018-12-06 13:07:53 +0100

    Initial commit

---
+++ app/src/main/java/com/example/corne/trivia/GamePlayActivity.java
@@ -0,0 +1,13 @@
+package com.example.corne.trivia;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GamePlayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gameplay);
+    }
+}
